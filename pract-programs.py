17980bf28e2d315f632845966f70bd4d6474662c
Aditya Hegde <91192289+xadityahx@users.noreply.github.com>
2021-12-31 00:33:25 +0530

    Formated

--- pract-programs.py
+++ pract-programs.py
@@ -1,7 +1,7 @@
-from os import makedirs
-
-
-def pick_max():
+# pick max or min value in a list or tuple
+def max_min():
+    
+    # WRITE CODE FROM HERE    
     lt = eval(input("Enter a list or a tuple : "))
     mm = input("Do you want Max(1) or Min(2) :")
     typ = "list" if type(lt) == list else "tuple"
@@ -12,7 +12,10 @@ def pick_max():
     else:
         return print("Invalid Operation")
 
+# swap values in even positions with those in odd positions
 def eo_swap():
+    
+    # WRITE CODE FROM HERE    
     l = list(eval(input("Enter a list with numbers : ")))
     eo = []
     for i in range(0, len(l), +2):
@@ -22,7 +25,10 @@ def eo_swap():
             eo.extend([l[i+1], l[i]])
     return eo
 
+# search a string for a specific word and return its index
 def search():
+    
+    # WRITE CODE FROM HERE    
     lt = eval(input("Enter a list or tuple : "))
     sr = input("Search element : ")
     indexes = []
@@ -30,15 +36,23 @@ def search():
         if str(lt[i]) == sr:
             indexes.append(i)
     return print("The element", sr, "has indexes", indexes)
-    
+
+# print the squares of numbers from 1 to 10
 def sqto10():
+    
+    # WRITE CODE FROM HERE
     return [x**2 for x in range(11)]
 
+# return the first letter of each word in a string
 def first_letter():
+    # WRITE CODE FROM HERE
     string = input("Enter a string : ").split(' ')
     return [x[0] for x in string]
 
+# Push the duplicate elements in a list to the very end
 def punish():
+    
+    # WRITE CODE FROM HERE
     l = list(eval(input("Enter a list with elements : ")))
     d = {}
     ln = []
@@ -55,7 +69,10 @@ def punish():
                 ln.extend([k])
     return ln
 
+# Return the indexes of the places where two similar lists differ
 def differ():
+    
+    # WRITE CODE FROM HERE
     l1 = list(eval(input("Enter list 1 : ")))
     l2 = list(eval(input("Enter list 2 : ")))
 
@@ -65,18 +82,22 @@ def differ():
                 print("Differing point index :", i)
 
 # Print name of student getting above 75
-cl = {'roll':[1, 2, 3, 4, 5], 'name':['Name1', 'Name2', 'Name3', 'Name4', 'Name5'],\
-    'marks':[82, 75, 60, 90, 92]}
 def above75(d=dict):
+    # WRITE CODE FROM HERE
+    
+    cl = {'roll':[1, 2, 3, 4, 5], 'name':['Name1', 'Name2', 'Name3', 'Name4', 'Name5'],\
+    'marks':[82, 75, 60, 90, 92]}
     for i in range(0, len(d['marks']), +1):
         if d['marks'][i] > 75:
             print(d['name'[i]])
 
-d1 = {1: [1, 4, 'Apple', 15.2], 2:"Banana", 3:True, 4:15+2j}
-d2 = {1:"Orange", 2:[[24, 12], 2, [1.0, 2E7]]}
-d3 = {1:"Banana", 2:True, 3:[1, 4, 'Apple', 15.2], 4:15+2j}
-
+# compare two dictionaries and check if they are similar or not
 def chk_d(dict1, dict2):
+    
+    # WRITE CODE FROM HERE
+    d1 = {1: [1, 4, 'Apple', 15.2], 2:"Banana", 3:True, 4:15+2j}
+    d2 = {1:"Orange", 2:[[24, 12], 2, [1.0, 2E7]]}
+    d3 = {1:"Banana", 2:True, 3:[1, 4, 'Apple', 15.2], 4:15+2j}
     lc = []
     l = []
     for i in dict1:
@@ -88,8 +109,11 @@ def chk_d(dict1, dict2):
             return print("The two dictionaries are similar")
         else:
             return print("The two dictionaries are dissimilar")
-
+        
+# count the number of times a word occurs in a string
 def count_occur():
+    
+    # WRITE CODE FROM HERE
     s = input("Enter a string : ").lower()
     chk = input("Word count of which word : ").lower()
     return print("The word", chk, "occurs in the string", s.count(chk), "times.")
